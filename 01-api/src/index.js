14864a5d8591342de1766d20a1739a1b6302fad8
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2019-03-01 16:12:24 +0200

    Added new routes to app

--- 01-api/src/index.js
+++ 01-api/src/index.js
@@ -7,6 +7,11 @@ const config = require('./utils/config')
 const mongoose = require('mongoose')
 
 const coolingUnitsRouter = require('./controllers/coolingUnits')
+const devicesRouter = require('./controllers/devices')
+const fansRouter = require('./controllers/fans')
+const fanServersRouter = require('./controllers/fanServers')
+const temperatureSensorsRouter = require('./controllers/temperatureSensors')
+const temperatureServersRouter = require('./controllers/temperatureServers')
 
 
 //Set up middleware
@@ -14,18 +19,26 @@ app.use(bodyParser.json())
 app.use(middleware.logger())
 
 //Set up routers
-app.use('/api/coolingunit', coolingUnitsRouter)
+app.use('/coolingunit', coolingUnitsRouter)
+app.use('/device', devicesRouter)
+app.use('/fan', fansRouter)
+app.use('/fanserver', fanServersRouter)
+app.use('/temperature', temperatureSensorsRouter)
+app.use('/temperatureserver', temperatureServersRouter)
 
 //If no routes match, send error message
 app.use(middleware.error())
 
-
+// Set up database connection
 mongoose
-    .set('useFindAndModiyf', false)
+    .set('useFindAndModify', false)
     .connect(config.mongoUrl, { useNewUrlParser: true })
-    .then(x => console.log('Connected to database', x))
+    .then(console.log('Connected to database'))
+    .then(console.log(`Mongoose version ${mongoose.version}`))
     .catch(err => console.log(err))
 
+
+// Create and set up server
 const server = http.createServer(app)
 
 server.listen(config.port, () => {
