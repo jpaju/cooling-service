58e1860946e1a8c09f939298dc6efdc9d55c7cee
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2019-03-15 01:06:26 +0200

    Added validation when creating single fan

--- api/src/controllers/fans.js
+++ api/src/controllers/fans.js
@@ -1,5 +1,6 @@
 const fansRouter = require('express').Router()
 const Fan = require('../models/fan')
+const FanServer = require('../models/fanServer')
 
 
 fansRouter.get('/', async(request, response) => {
@@ -19,20 +20,68 @@ fansRouter.get('/:id', async(request, response) => {
 })
 
 fansRouter.post('/', async(request, response) => {
-    const { body } = request
+    const { server, pinNumber, ...optionalData } = request.body
 
-    if (!body.server) {
-        return response.status(400).send({ error: 'Fan-server required required' })
+    if (!server || !pinNumber) {
+        return response.status(400).send({ error: 'Fan-server and pin number required' })
     }
 
-    const newFan = new Fan({
-        name: body.name,
-        devices: body.devices,
-        fans: body.fans
-    })
-    await newFan.save()
+    const fanServer = await FanServer
+        .findById(server)
+        .catch(() => null /* Prevent raising error */)
 
-    response.status(201).json(Fan.format(newFan))
+    if (!fanServer) {
+        return response.status(400).send({ error: 'Fan-server not found' })
+    }
+
+    // Check that fan server is able to create fan on specified pin
+    if (!fanServer.fanPins.includes(pinNumber)) {
+        return response.status(400).send({ error: `Pin number ${pinNumber} is not valid` })
+    }
+
+    // If fan already exists, update speed & frequency and return updated fan
+    const existingFanData = fanServer.fans.find(fan => fan.pinNumber === pinNumber)
+    if (existingFanData) {
+        const existingFan = await Fan
+            .findOneAndUpdate(
+                { pinNumber: existingFanData.pinNumber, server: fanServer },
+                optionalData,
+                { new: true }
+            )
+
+        return response.json(Fan.format(existingFan))
+    }
+
+    // Use values provided in the request, or use defaults values
+    // from the server if no values are specified
+    const speed = optionalData.speed || fanServer.defaults.speed
+    const frequency = optionalData.frequency || fanServer.defaults.frequency
+
+    const newFan = new Fan({
+        pinNumber,
+        speed,
+        frequency,
+        server: fanServer
+    })
+
+    // Add new fan to fan server and store both to database
+    fanServer.fans = fanServer.fans.concat(newFan)
+    await Promise.all([
+        newFan.save(),
+        fanServer.save()
+    ])
+
+    Fan.populate(
+        newFan,
+        {
+            path: 'server',
+            select: 'url -fans'
+        },
+        function(err, fan) {
+            response.status(201).json(Fan.format(fan))
+        }
+    )
+
 })
 
 fansRouter.put('/:id', async(request, response) => {
