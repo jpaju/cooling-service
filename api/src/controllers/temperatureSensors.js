a2b855f692639a0de997433e89692475de9153b2
Jaakko Paju <36770267+JPaju@users.noreply.github.com>
2019-03-14 22:30:44 +0200

    Added ability to request unassigned sensors

--- api/src/controllers/temperatureSensors.js
+++ api/src/controllers/temperatureSensors.js
@@ -1,8 +1,26 @@
 const temperatureSensorsRouter = require('express').Router()
+const boolParser = require('express-query-boolean')
 const TemperatureSensor = require('../models/temperatureSensor')
+const Device = require('../models/device')
+
+temperatureSensorsRouter.use(boolParser())
 
 
 temperatureSensorsRouter.get('/', async(request, response) => {
+    const { unassigned } = request.query
+
+    // Find temperature sensors that are not assigned to any device
+    if (unassigned) {
+        const devicesWithSensors = await Device
+            .find({ temperatureSensor: { '$exists' : true, '$ne': null } })
+
+        const assignedSensorIds = devicesWithSensors.map(device => device.temperatureSensor._id)
+        const temperatureSensors = await TemperatureSensor
+            .find({ _id: { '$nin': assignedSensorIds } })
+
+        return response.json(temperatureSensors.map(TemperatureSensor.format))
+    }
+
     const temperatureSensors = await TemperatureSensor.find({})
     response.json(temperatureSensors.map(TemperatureSensor.format))
 })
